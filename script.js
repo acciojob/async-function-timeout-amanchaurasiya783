3edc3eccedbfe11570aa88c08a46bd0dde6ec0c4
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-28 22:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const submitBtn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+submitBtn.addEventListener('submit', async function(e){
+	e.preventDefault();
+	const text = document.getElementById('text');
+	const delay = document.getElementById('delay');
+	const result = await displayMsg;
+	output.innerHTML = result;
+})
+
+function displayMsg(text, delay){
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve(text);
+		}, delay);
+	})
+}
